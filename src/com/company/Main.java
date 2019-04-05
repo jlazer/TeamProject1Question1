e8c70ef406cbd2b617c29c3bce4514563383edcd
Justin Lazarski <blackhawks189@gmail.com>
2019-04-04 21:43:16 -0500

    First Commit.

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+/* Justin Lazarski
+  Jonathan La Rosa
+  CIS 3090.04
+  Project 1
+  Question 1
+*/
+package com.company;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please input the month and day.");
+
+    }
+}
